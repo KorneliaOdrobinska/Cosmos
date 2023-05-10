fd2e9a7d58a64a40f48a3a76d5dcc3a7b1fc1517
KorneliaOdrobinska <kornelia.odrobinska@gmail.com>
2023-05-10 21:36:47 +0200

    Add Mission

--- src/main/java/pl/odrobinska/cosmos/Mission.java
+++ src/main/java/pl/odrobinska/cosmos/Mission.java
@@ -1,2 +1,63 @@
-package pl.odrobinska.cosmos;public class Mission {
+package pl.odrobinska.cosmos;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "MISSIONS")
+public class Mission {
+    @Id
+    @GeneratedValue(generator="inc")
+    @GenericGenerator(name="inc", strategy = "increment")
+    private Integer ID;
+    private String name;
+    private Boolean manned;
+    private Integer celestialBodyCorrelation;
+    private Integer satelliteCorrelation;
+    private String missionStartdate; // TODO change to date type
+    private Boolean isFinished;
+
+    public Mission(String name, Boolean manned, Integer celestialBodyCorrelation, Integer satelliteCorrelation, String date, Boolean isFinished) {
+        this.name = name;
+        this.manned = manned;
+        this.celestialBodyCorrelation = celestialBodyCorrelation;
+        this.satelliteCorrelation = satelliteCorrelation;
+        this.missionStartdate = date;
+        this.isFinished = isFinished;
+    }
+
+    public Integer getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Boolean isManned() {
+        return manned;
+    }
+
+    public Integer getCelestialBodyCorrelation() {
+        return celestialBodyCorrelation;
+    }
+
+    public Integer getSatelliteCorrelation() {
+        return satelliteCorrelation;
+    }
+
+    public String getMissionStartdate() {
+        return missionStartdate;
+    }
+
+    public Boolean getFinished() {
+        return isFinished;
+    }
+
+    public void setFinished(Boolean finished) {
+        isFinished = finished;
+    }
 }
